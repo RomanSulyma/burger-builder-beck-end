d6eb479cd1a83c4f7718fc299429568fa168b8d9
Roman <roman.sulima777@gmail.com>
2020-07-01 20:36:06 +0300

    fixed timezone use to UTC time

--- src/main/java/com/net/burgerbuilder/controller/AuthController.java
+++ src/main/java/com/net/burgerbuilder/controller/AuthController.java
@@ -20,8 +20,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -102,7 +102,7 @@ public class AuthController {
     loginResponse.setResponseMessage("Login successful");
     loginResponse.setUsername(authentication.getName());
     loginResponse.setRoles(authentication.getAuthorities().toString());
-    loginResponse.setExpirationTime(LocalDateTime.now().plusSeconds(expirationTime).toString());
+    loginResponse.setExpirationTime(LocalDateTime.now().atZone(ZoneId.of("UTC")).plusSeconds(expirationTime).toString());
     loginResponse.setToken(jwt);
 
     return new ResponseEntity<>(loginResponse, HttpStatus.OK);
