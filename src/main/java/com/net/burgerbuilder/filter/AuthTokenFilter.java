de573a2da6f06b05df9817f2d1a2ea42f2dabcc5
Roman <roman.sulyma97@gmail.com>
2020-08-24 16:41:29 +0300

    Some updates

--- src/main/java/com/net/burgerbuilder/filter/AuthTokenFilter.java
+++ src/main/java/com/net/burgerbuilder/filter/AuthTokenFilter.java
@@ -32,13 +32,14 @@ public class AuthTokenFilter extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
           throws ServletException, IOException {
     try {
-      String jwt = parseJwt(request);
+      final String jwt = parseJwt(request);
       if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
-        String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+        final String username = jwtUtils.getUserNameFromJwtToken(jwt);
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                 userDetails, null, userDetails.getAuthorities());
+
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
